2c4dba4d5969485af0f8a3f5504a196d63378f82
Stijn Heymans <stijn.heymans@gmail.com>
2014-10-06 16:28:55 -0700

    Added some invite tests

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import resolve
-from users.models import UserProfile
+from users.models import UserProfile, Invite
 from django.contrib.auth.models import User
 from django.conf import settings
 
@@ -8,6 +8,10 @@ class ViewTest(TestCase):
     def setUp(self):
 
         self.client_stub = Client()
+
+    def tearDown(self):
+        Invite.objects.all().delete()
+        
 
     def test_post_signup_has_to_be_trial_user(self):
         # when someone signs up, he has to be a trial user by default
@@ -23,6 +27,40 @@ class ViewTest(TestCase):
 
         # clean up get rid of user again
         User.objects.filter(username='trial_test_signup_1').delete()
+ 
 
+    def test_invite_already_a_user(self):
+        self.user = User.objects.create_user('lennon', 'lennon@thebeatles.com', 'johnpassword')
+        # trial user:
+        self.userprofile = UserProfile.objects.create(user=self.user, usertype=1, timezone="America/Los_Angeles")
+        self.client_stub.login(username='lennon', password='johnpassword')
 
+        response = self.client_stub.post('/users/invite/', {'email': 'lennon@thebeatles.com'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_invite_already_invited(self):
+        self.user = User.objects.create_user('lennon', 'lennon@thebeatles.com', 'johnpassword')
+        # trial user:
+        self.userprofile = UserProfile.objects.create(user=self.user, usertype=1, timezone="America/Los_Angeles")
+        self.client_stub.login(username='lennon', password='johnpassword')
+
+        Invite.objects.create(inviter=self.user, invited='paul@thebeatles.com')
+        self.assertTrue(Invite.objects.filter(invited__iexact='paul@thebeatles.com').exists())
+
+        response = self.client_stub.post('/users/invite/', {'email': 'paul@thebeatles.com'})
+        self.assertEqual(response.status_code, 200)
+
+        # does not get added 2 times
+        self.assertTrue(Invite.objects.filter(invited__iexact='paul@thebeatles.com').count() == 1)
+
+    def test_invite_successful_invite(self):
+        self.user = User.objects.create_user('lennon', 'lennon@thebeatles.com', 'johnpassword')
+        # trial user:
+        self.userprofile = UserProfile.objects.create(user=self.user, usertype=1, timezone="America/Los_Angeles")
+        self.client_stub.login(username='lennon', password='johnpassword')
+
+        response = self.client_stub.post('/users/invite/', {'email': 'paul@thebeatles.com'})
+        # should be a redirect
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Invite.objects.filter(invited__iexact='paul@thebeatles.com').exists())
 
