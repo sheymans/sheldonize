299455d8c3cfb35f98b07d0a5d2efbfd3b2d8e23
Stijn Heymans <stijn.heymans@gmail.com>
2014-12-15 11:28:22 -0800

    Lighter color for past foreign meetings

--- app/service.py
+++ app/service.py
@@ -103,7 +103,9 @@ def meetings_2_dict(meetings, user_timezone):
         # send ISO08601 back to front-end
         jso["start"] = start.datetime.isoformat()
         jso["end"] = end.datetime.isoformat()
-        if meeting.foreign is not None:
+        if meeting.foreign is not None and in_past:
+            jso["color"] = eventcolors.meeting_foreign_past["color"]
+        elif meeting.foreign is not None:
             jso["color"] = eventcolors.meeting_foreign["color"]
         elif in_past:
             jso["color"] = eventcolors.meeting_past["color"]
